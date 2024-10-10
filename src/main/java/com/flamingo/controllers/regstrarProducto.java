31e14e1b33866d7e0ae54266ef1417085312c4a4
joaqui-jz <164902689+joaqui-jz@users.noreply.github.com>
2024-10-10 18:47:43 -0300

    Push

---
+++ src/main/java/com/flamingo/controllers/regstrarProducto.java
@@ -0,0 +1,107 @@
+package com.flamingo.controllers;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import com.flamingo.exceptions.CategoriaNoExisteException;
+import com.flamingo.exceptions.ProductoNoExisteException;
+import com.flamingo.exceptions.UsuarioNoExisteException;
+import com.flamingo.models.EstadoSesion;
+import com.flamingo.models.ISistema;
+import com.flamingo.models.Producto;
+import com.flamingo.models.SistemaFactory;
+import com.flamingo.models.Usuario;
+import com.flamingo.models.Categoria;
+
+@WebServlet ("/registrarProducto")
+public class registrarProducto extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+    public registrarProducto() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	public static EstadoSesion getEstado(HttpServletRequest request)
+	{
+		return (EstadoSesion) request.getSession().getAttribute("estado_sesion");
+	}
+
+	private void processRequest(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException, ProductoNoExisteException, CategoriaNoExisteException {
+		ISistema sis = SistemaFactory.getInstancia().getISistema();
+		sis.crearCasos();
+	
+		request.setAttribute("usuarioActual", sis.getUsuarioActual());
+		request.setAttribute("categorias", sis.getCategorias());
+		Object usuario = request.getAttribute("usuarioActual");
+		Object categorias = request.getAttribute("categorias");	
+		
+		if(usuario == null) {
+			request.setAttribute("usuarioActual", null);
+			
+			request.getRequestDispatcher("/WEB-INF/products/infoProducto.jsp").
+					forward(request, response);
+		} else {
+			Usuario usr = (Usuario) usuario;
+			request.setAttribute("usuarioActual", usr);
+			
+			HashMap<String, Categoria> ctg = (HashMap<String, Categoria>) categorias;
+			request.setAttribute("categorias", ctg);
+			
+
+			request.getRequestDispatcher("/WEB-INF/products/infoProducto.jsp").
+					forward(request, response);
+		}
+	}
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {
+			try {
+				processRequest(request, response);
+			} catch (CategoriaNoExisteException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} catch (ServletException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ProductoNoExisteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {
+			try {
+				processRequest(request, response);
+			} catch (CategoriaNoExisteException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} catch (ServletException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ProductoNoExisteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
