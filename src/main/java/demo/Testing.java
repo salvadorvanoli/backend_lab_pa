5df4ef8b43e59acb53e2d55867524ab9644d6fe7
kingstolas12 <164377072+kingstolas@users.noreply.github.com>
2024-10-14 21:36:58 -0300

    testing

--- src/main/java/demo/Testing.java
+++ src/main/java/demo/Testing.java
@@ -4,11 +4,46 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import com.flamingo.exceptions.ContraseniaIncorrectaException;
+import com.flamingo.exceptions.UsuarioRepetidoException;
+import com.flamingo.models.DTFecha;
+import com.flamingo.models.ISistema;
+import com.flamingo.models.SistemaFactory;
+
 class Testing {
 
-	@Test
-	void test() {
-		System.out.println("aaaaaa");
+	public Testing() {
+		super();
 	}
+
+	@Test
+    public void testAltaUsuarioProveedorExitoso() throws UsuarioRepetidoException, ContraseniaIncorrectaException {
+        ISistema sis = SistemaFactory.getInstancia().getISistema();
+        DTFecha fecha = new DTFecha(12, 06, 1978);
+        String contra = "12345678";
+        String ruta = "/hola/jeje";
+        
+            boolean res = sis.altaUsuarioProveedor("nickname1", "email1@gmail.com", "nombreuno", "apellidouno", fecha, "CompaniaUno", "https://www.uno.com", ruta, contra, contra);
+            assertEquals(true, res);
+        
+   }
+    
+	 @Test
+	    public void testAltaUsuarioProveedorConNicknameRepetido() throws UsuarioRepetidoException, ContraseniaIncorrectaException {
+	        // Instancia del sistema
+	        ISistema sis = SistemaFactory.getInstancia().getISistema();
+	        DTFecha fecha = new DTFecha(12, 06, 1978);
+
+	        // Se agrega un proveedor con un nickname existente
+	        //boolean res = sis.altaUsuarioProveedor("nickname1", "email1@gmail.com", "nombreuno", "apellidouno", fecha, "CompaniaUno", "https://www.uno.com", null, "12345678", "12345678");
+	        //assertEquals(res, true);
+	        
+	        
+	        // Intentar registrar otro usuario con el mismo nickname
+	       
+	        boolean ress = sis.altaUsuarioProveedor("nickname2", "email2@gmail.com", "nombredos", "apellidodos", fecha, "CompaniaDos", "https://www.dos.com", null, "12345678", "12345678");
+	        boolean res2 = sis.altaUsuarioCliente("nickname2", "email2@gmail.com", "nombre", "apellidooss", fecha, "/aa/a", "123456789", "123456789");
+	        assertEquals(false, res2);
+	    }
 
 }
