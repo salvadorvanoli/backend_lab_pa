4ed7e2dbccaa5579e2903b0954485a363d25f022
Blckdev <23felipecamejo@gmail.com>
2024-10-15 22:42:30 -0300

    testing

--- src/main/java/demo/Testing.java
+++ src/main/java/demo/Testing.java
@@ -2,13 +2,24 @@ package demo;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import com.flamingo.exceptions.CategoriaNoExisteException;
+import com.flamingo.exceptions.CategoriaNoPuedeTenerProductosException;
+import com.flamingo.exceptions.CategoriaRepetidaException;
 import com.flamingo.exceptions.ContraseniaIncorrectaException;
+import com.flamingo.exceptions.OrdenDeCompraNoExisteException;
+import com.flamingo.exceptions.OrdenDeCompraRepetidaException;
+import com.flamingo.exceptions.ProductoNoExisteException;
 import com.flamingo.exceptions.UsuarioRepetidoException;
+import com.flamingo.models.Categoria;
 import com.flamingo.models.DTFecha;
 import com.flamingo.models.ISistema;
+import com.flamingo.models.Producto;
 import com.flamingo.models.SistemaFactory;
 
 class Testing {
@@ -97,7 +108,176 @@ class Testing {
         assertEquals("Ya existe un usuario registrado con el nickname \"nicknameRepetir\".", thrown.getMessage());
     }
 	
-
-
-
+    @Test
+    public void ElegirOrdenNoExiste() {
+    	// Instancia del sistema
+    	ISistema sis = SistemaFactory.getInstancia().getISistema();
+    	
+    	int num = 100000000;
+    	
+    	// intento elegir una orden con un numero de orden que no existe
+    	OrdenDeCompraNoExisteException thrown = assertThrows(OrdenDeCompraNoExisteException.class, () ->{ 
+    		sis.elegirOrdenDeCompra(num);
+    		//no deberia ejecutarse
+    	});
+    	
+    	// Verificar el mensaje de la excepción
+    	assertEquals("La orden de compra número " + '"' + num + '"' + " no existe.", thrown.getMessage());
+    }
+    
+    @Test
+    public void CancelarOrdenNoExiste() {
+    	// Instancia del sistema
+    	ISistema sis = SistemaFactory.getInstancia().getISistema();
+    	
+    	int num = 100000000;
+    	
+    	// intento elegir una orden con un numero de orden que no existe
+    	OrdenDeCompraNoExisteException thrown = assertThrows(OrdenDeCompraNoExisteException.class, () ->{ 
+    		sis.cancelarOrdenDeCompra(num);
+    		//no deberia ejecutarse
+    	});
+    	
+    	// Verificar el mensaje de la excepción
+    	assertEquals("La orden de compra número " + '"' + num + '"' + " no existe.", thrown.getMessage());
+    }
+    
+   /* @Test
+    public void OrdenDeCompraRepetida() {
+    	// Instancia del sistema
+    	ISistema sis = SistemaFactory.getInstancia().getISistema();
+    	int num = 100000000;
+    	
+    	 try {
+             sis. //aca iria algo que creeoRdenes y tire la escepcion de OrdenDeCompraRepetidaException
+         } catch (Exception e) {
+             fail("No se debería haber lanzado una excepción."); //no deberia ejecutarse
+         }
+    	
+    	// intento elegir una orden con un numero de orden que no existe
+    	OrdenDeCompraRepetidaException thrown = assertThrows(OrdenDeCompraRepetidaException.class, () ->{ 
+    		System.out.println("AAA");
+    		sis.elegirOrdenDeCompra(num);
+    		// no deberia ejecutarse
+    	});
+    	
+    	// Verificar el mensaje de la excepción
+    	assertEquals("La Orden De compra y existe: ", thrown.getMessage());
+    }
+	*/
+    
+    @Test
+    public void CategoriaNoExisteConNombre() {
+    	// Instancia del sistema
+    	ISistema sis = SistemaFactory.getInstancia().getISistema();
+    	
+    	String nom = "vamoarribaeldefensorsportingparasiempreyporsiempre";
+    	
+		CategoriaNoExisteException thrown = assertThrows(CategoriaNoExisteException.class, () ->{ 
+    		sis.elegirCategoria(nom);
+    		//no deberia ejecutarse
+    	});
+		
+		assertEquals("La categoría de nombre " + '"' + nom + '"' + " no existe.", thrown.getMessage());
+    }
+    
+    @Test
+    public void CategoriaNoExisteConNUll() {
+    	// Instancia del sistema
+    	ISistema sis = SistemaFactory.getInstancia().getISistema();
+    	
+    	String nom = null;
+    	
+    	CategoriaNoExisteException thrown = assertThrows(CategoriaNoExisteException.class, () ->{ 
+    		sis.elegirCategoria(nom);
+    		//no deberia ejecutarse
+    	});
+		
+		assertEquals("La categoría de nombre " + '"' + nom + '"' + " no existe.", thrown.getMessage());
+    	
+    }
+    
+    @Test
+    public void agregarProductoACategoriaSinProductosConProdActualNull() {
+    	// Instancia del sistema
+    	ISistema sis = SistemaFactory.getInstancia().getISistema();
+    	
+    	// Instacncia de categoria
+    	Categoria cat = new Categoria("test12sisistesteado", false, null);
+    	
+		try {
+			sis.altaCategoria(cat.getNombreCat(), false, cat.getPadre());
+		} catch (CategoriaRepetidaException e) {
+			fail("No se debería haber lanzado una excepción."); //no deberia ejecutarse
+		}
+		
+    	List<Categoria> lista = new ArrayList<>();
+    	
+    	lista.add(cat);
+    	
+    	NullPointerException thrown = assertThrows(NullPointerException.class, () ->{ 
+    		sis.agregarProductoACategorias(lista);
+    		//no deberia ejecutarse
+    	});
+    	
+    	assertEquals("No se ha elegido un producto previamente.", thrown.getMessage());
+    }
+    
+    @Test
+    public void agregarProductoACategoriaSinProductosConProdActual() {
+    	// Instancia del sistema
+    	ISistema sis = SistemaFactory.getInstancia().getISistema();
+    	
+    	// Instacncia de categoria
+    	Categoria cat = new Categoria("test1234testsisistesteado", false, null);
+    	
+		try {
+			sis.altaCategoria(cat.getNombreCat(), false, cat.getPadre());
+		} catch (CategoriaRepetidaException e) {
+			fail("No se debería haber lanzado una excepción."); //no deberia ejecutarse
+		}
+		
+    	List<Categoria> lista = new ArrayList<>();	
+    	lista.add(cat);
+    	
+    	Producto prod = new Producto("wattpad", "descrip", null, 81118890, 0, null, lista, null, "dd market");
+    	
+    	
+    	
+    	try {
+			sis.elegirCategoria(cat.getNombreCat());
+		} catch (CategoriaNoExisteException e) {
+			fail("No se debería haber lanzado una excepción."); //no deberia ejecutarse
+		}
+    	
+    	try {
+			sis.elegirProducto(prod.getNombreProducto());
+		} catch (ProductoNoExisteException e) {
+			fail("No se debería haber lanzado una excepción."); //no deberia ejecutarse
+		}
+    	
+    	CategoriaNoPuedeTenerProductosException thrown = assertThrows(CategoriaNoPuedeTenerProductosException.class, () ->{ 
+    		sis.agregarProductoACategorias(lista);
+    		//no deberia ejecutarse
+    	});
+    	
+    	assertEquals("Las categorías " + cat.getNombreCat() + " no puede contener productos.", thrown.getMessage());
+    }
+    
+    @Test
+    public void agregarCategoriaAProductoACategoriaSinProductos() {
+    	// Instancia del sistema
+    	ISistema sis = SistemaFactory.getInstancia().getISistema();
+    	
+    	
+    }
+    
+    @Test
+    public void CategoriaRepetida() {
+    	// Instancia del sistema
+    	ISistema sis = SistemaFactory.getInstancia().getISistema();
+    	
+    }
+    
+    
 }
